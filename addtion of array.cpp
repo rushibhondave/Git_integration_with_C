cf3824c94bca618326ee5d4c0638a38c56ef9d84
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-17 22:05:54 +0530

    Basic Array C Program

---
+++ addtion of array.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+int i,sum;
+int marks[5]={10,12,13,14,15};
+	
+for(i=0;i<5;i++)
+{
+	
+printf("%i\t",marks[i]);
+
+}
+ 
+ 
+ for(i = 0; i<5; i++)
+{
+
+  sum = sum + marks[i];
+   
+}
+
+
+
+printf("\naddtion of array:%i",sum);
+}
